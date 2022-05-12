b93df6b80bf6003b13370adb0d0333ab90ee3a60
Lintang Hannan M <lintanghannan123@gmail.com>
2022-05-12 08:14:00 +0700

    Belajar

--- script.sh
+++ script.sh
@@ -10,3 +10,11 @@ docker build -t node-app-image .
 #jalankan container
 docker run -p 3000:3000 -d --name node-app node-app-image
 
+#Masuk ke bash container
+docker exec -it node-app bash
+
+#Cara mengambil file
+docker run -v (pwd):/app -p 3000:3000 -d --name node-app node-app-image                                                                                                                                                   (main*) 20:17:18
+
+#Menambahkan nodemon 
+npm install nodemon --save-dev
