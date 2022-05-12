994fbc1c09e0a31dc69a70afcb15801a05b9f422
Lintang Hannan M <lintanghannan123@gmail.com>
2022-05-13 02:07:08 +0700

    Belajar

--- script.sh
+++ script.sh
@@ -14,7 +14,28 @@ docker run -p 3000:3000 -d --name node-app node-app-image
 docker exec -it node-app bash
 
 #Cara mengambil file
-docker run -v (pwd):/app -p 3000:3000 -d --name node-app node-app-image                                                                                                                                                   (main*) 20:17:18
+docker run -v (pwd):/app -p 3000:3000 -d --name node-app node-app-image
 
 #Menambahkan nodemon 
 npm install nodemon --save-dev
+
+#Coba env menggunakan port berbeda 
+docker run -v (pwd):/app:ro -v /app/node_modules --env PORT=4000 -p 3000:4000 -d --name node-app node-app-image
+
+#Membuat docker compose
+docker-compose up -d
+
+#Shutdown docker-compose dan hapus data anynomous
+docker-compose down -v
+
+#membuat build baru dari docker compose
+docker-compose up -d --build
+
+#up docker compose dev
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+
+#up docker compose prod
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+
+#up docker compose prod (update)
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
